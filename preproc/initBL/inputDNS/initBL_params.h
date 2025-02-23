edb6eb98e5a831b2a893fe4bfb56f9977d10b010
Pietro Carlo Boldini <p.c.boldini@tudelft.nl>
2025-02-23 22:48:24 +0100

    update initBL

--- preproc/initBL/inputDNS/initBL_params.h
+++ preproc/initBL/inputDNS/initBL_params.h
@@ -1,17 +1,19 @@
 !------------ use equation of state ------------------ 
 USE_EOS  = "IG" 
 !------------ set non-dimensional free-stream values for computation ------------------ 
-delta99   = 4.9280150919 
-Pra   = 0.7500000000 
+delta99   = 4.9982781155 
+Pra   = 0.7200000000 
 Ec   = 1.6000000000e-02 
 Ma   = 2.0000000000e-01 
 Pref   = 17.857143 
 ig_gam   = 1.400000 
 eos_Rgas   = 17.857143 
 eos_dof   = 9.000000 
+Ri_wall   = -0.010000 
+Ri_unit   = 0.210000 
 ! ------------ set wall BC for computation ------------------ 
-wall_bc  = "adiab" 
-Twall_bot   = -1.000000 
+wall_bc  = "isoth" 
+Twall_bot   = 1.050000 
 ! ----------- set viscosity and conductivity ---------------- 
 USE_VISC  = "Sutherland" 
 Smuref   = 111.000000 
